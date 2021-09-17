2c309d81f6932d259b12a4ae9e584922e11d63eb
Lakshay Sachdeva <lakshay07sachdeva@gmail.com>
2021-09-17 09:52:41 +0530

    home update

---
+++ flipkart/home_body.js
@@ -0,0 +1,170 @@
+function home_body() {
+    return `<div id="navigation">
+            <figure>
+                <img src = "images-lakshay/top_offers.png" />
+                <figcaption>Top Offers</figcaption>
+            </figure>
+            <figure>
+                <img src = "images-lakshay/grocery.png" />
+                <figcaption>Grocery</figcaption>
+            </figure>
+            <a href="dum.html" >
+                <figure>
+                    <img src = "images-lakshay/mobiles.png" />
+                    <figcaption>Mobiles</figcaption>
+                </figure>
+            </a>
+            <figure>
+                <img src = "images-lakshay/fashion.png" />
+                <figcaption>Fashion</figcaption>
+            </figure>
+            <figure>
+                <img src = "images-lakshay/electronics.png" />
+                <figcaption>Electronics</figcaption>
+            </figure>
+            <figure>
+                <img src = "images-lakshay/home.jpg" />
+                <figcaption>Home</figcaption>
+            </figure>
+            <figure>
+                <img src = "images-lakshay/appliances.png" />
+                <figcaption>Appliances</figcaption>
+            </figure>
+            <figure>
+                <img src = "images-lakshay/travel.png" />
+                <figcaption>Travel</figcaption>
+            </figure>
+            <figure>
+                <img src = "images-lakshay/beauty_toys.png" />
+                <figcaption>Beauty, Toys & More</figcaption>
+            </figure>
+        </div>
+        <hr/>
+        <div id = "courosel"></div>
+        <hr/>
+        <div id="deals_of_day">
+            <div class="rows">                  
+                <p>Deal of the Day</p>
+                <button>View All</button>
+            </div>
+            <div id = "day_deals" class="rows">
+                <figure>
+                    <img src="images-lakshay/dod1.jpeg"/>
+                    <figcaption>Bedsheets</figcaption>
+                </figure>
+                <figure>
+                    <img src="images-lakshay/dod2.jpg"/>
+                    <figcaption>Top Selling Men's Accessories</figcaption>
+                </figure>
+                <figure>
+                    <img src="images-lakshay/dod3.jpeg"/>
+                    <figcaption>Electric Appliances</figcaption>
+                </figure>
+                <figure>
+                    <img src="images-lakshay/dod4.jpeg"/>
+                    <figcaption>Shoe Racks & more</figcaption>
+                </figure>
+                <figure>
+                    <img src="images-lakshay/dod5.jpg"/>
+                    <figcaption>Sports & Fitness</figcaption>
+                </figure>
+                <figure>
+                    <img src="images-lakshay/dod6.jpeg"/>
+                    <figcaption>Electrical Hardware</figcaption>
+                </figure>
+                <figure>
+                    <img src="images-lakshay/dod7.jpg"/>
+                    <figcaption>Perfumes & deos</figcaption>
+                </figure>
+                <figure>
+                    <img src="images-lakshay/dod8.jpeg"/>
+                    <figcaption>Necklaces & Earings</figcaption>
+                </figure>
+            </div>
+        </div>
+        <hr/>
+        <div id="footwear_men">
+            <div id="footwear_men_head" class="rows">
+                <p>Men's Footwear</p>
+                <button>View All</button>
+            </div>
+            <div  class="rows">
+                <figure>
+                    <img src="images-lakshay/fm1.jpeg"/>
+                    <figcaption>Puma, Fila & more</figcaption>
+                </figure>
+                <figure>
+                    <img src="images-lakshay/fm2.jpeg"/>
+                    <figcaption>Best Selling Footwear</figcaption>
+                </figure>
+                <figure>
+                    <img src="images-lakshay/fm3.jpeg"/>
+                    <figcaption>Sparx, Bata & more</figcaption>
+                </figure>
+                <figure>
+                    <img src="images-lakshay/fm4.jpeg"/>
+                    <figcaption>Nike, New Balance</figcaption>
+                </figure>
+                <figure>
+                    <img src="images-lakshay/fm5.jpeg"/>
+                    <figcaption>Sparx, Paragon, Khadim's</figcaption>
+                </figure>
+                <figure>
+                    <img src="images-lakshay/fm6.jpeg"/>
+                    <figcaption>Ducati</figcaption>
+                </figure>
+                <figure>
+                    <img src="images-lakshay/fm7.jpeg"/>
+                    <figcaption>Campus, Yonex & more</figcaption>
+                </figure>
+                <figure>
+                    <img src="images-lakshay/fm8.jpeg"/>
+                    <figcaption>Nike, Skechers</figcaption>
+                </figure>
+            </div>
+        </div>
+        <hr/>
+        <div id="furniture">
+            <div id="furniture_head" class="rows">
+                <p>Futniture Bestsellers</p>
+                <button>View All</button>
+            </div>
+            <div class="rows">
+                <figure>
+                    <img src="images-lakshay/f1.jpeg"/>
+                    <figcaption>Hammock and Swings</figcaption>
+                </figure>
+                <figure>
+                    <img src="images-lakshay/f2.jpeg"/>
+                    <figcaption>Inflatable Sofas</figcaption>
+                </figure>
+                <figure>
+                    <img src="images-lakshay/f3.jpeg"/>
+                    <figcaption>Office and Study Chairs</figcaption>
+                </figure>
+                <figure>
+                    <img src="images-lakshay/f4.jpeg"/>
+                    <figcaption>Office and Study Tables</figcaption>
+                </figure>
+                <figure>
+                    <img src="images-lakshay/f5.jpeg"/>
+                    <figcaption>Wardrobes</figcaption>
+                </figure>
+                <figure>
+                    <img src="images-lakshay/f6.jpeg"/>
+                    <figcaption>TV Units</figcaption>
+                </figure>
+                <figure>
+                    <img src="images-lakshay/f7.jpeg"/>
+                    <figcaption>Sofa Sets</figcaption>
+                </figure>
+                <figure>
+                    <img src="images-lakshay/f8.jpeg"/>
+                    <figcaption>Hammock and Swings</figcaption>
+                </figure>
+            </div>
+        </div>
+        <hr/>`
+}
+
+export default home_body 
