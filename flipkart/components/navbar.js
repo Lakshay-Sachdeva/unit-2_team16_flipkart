e53eecc994c677cb5a8c7b0291c81170bf87f38e
Lakshay Sachdeva <lakshay07sachdeva@gmail.com>
2021-11-17 10:41:42 +0530

    navbar

---
+++ flipkart/components/navbar.js
@@ -0,0 +1,19 @@
+function navbar(){
+    return `
+    <div id="search-components">
+        <div>
+            <img src="./images-lakshay/flipkart-head.jpg" />
+        </div>
+        <div id="search">
+            <input type="text" placeholder="Search for Products, brands and more"  />
+            <img src="https://img.icons8.com/ios/50/000000/search--v1.png" />
+        </div>
+    </div>
+    <div id="user-info">
+        <div>Log In</div>
+        <div>More</div>
+        <div><img src="https://img.icons8.com/material/24/ffffff/fast-cart.png" />Cart</div>
+    </div>`
+}
+
+export default navbar ;
