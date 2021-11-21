a77f87c26e7b9c2a7526f865630fa463899f1997
Lakshay Sachdeva <lakshay07sachdeva@gmail.com>
2021-11-21 23:27:46 +0530

    anchor tags added

--- flipkart/components/navbar.js
+++ flipkart/components/navbar.js
@@ -1,18 +1,18 @@
 function navbar(){
     return `
     <div id="search-components">
-        <div>
+        <a href="home.html"><div>
             <img src="./images-lakshay/flipkart-head.jpg" />
-        </div>
+        </div></a>
         <div id="search">
             <input type="text" placeholder="Search for Products, brands and more"  />
             <img src="https://img.icons8.com/ios/50/000000/search--v1.png" />
         </div>
     </div>
     <div id="user-info">
-        <div>Log In</div>
+        <a href="login.html"><div>Log In</div></a>
         <div>More</div>
-        <div><img src="https://img.icons8.com/material/24/ffffff/fast-cart.png" />Cart</div>
+        <a href="cart.html"><div><img src="https://img.icons8.com/material/24/ffffff/fast-cart.png" />Cart</div></a>
     </div>`
 }
 
